9b3e44c3013dc1a3d236bcb11b26434273b224b7
Nabi <nabikaz@gmail.com>
2023-07-13 20:24:03 +0330

    Publishing the first version of the project.
    Signed-off-by: Nabi <nabikaz@gmail.com>

---
+++ README.md
@@ -0,0 +1,22 @@
+# Twitter Follower Count
+You know that in the web version of Twitter, to see the number of followers of a user, you can hold the mouse over his profile picture. But when the number of users is large, for example, when we receive many notifications from many users. or when you are browsing Twitter; Checking individual users is a time-consuming task.
+
+With this lightweight plugin, the number of followers is displayed automatically above the profile picture of each user without any clicks. So one look at the page is enough.
+
+Note that to avoid additional overhead, the code is executed after each scroll. So if nothing appears the first time, just scroll the mouse once.
+
+
+## Installation
+This is an userscript for `Tampermonkey`.
+
+First install [Tampermonkey](https://tampermonkey.net/) for `Chrome`, `Microsoft Edge`, `Safari`, `Firefox`, `Opera`, `Dolphin` or `UC Browser` from here: [https://tampermonkey.net/](https://tampermonkey.net/)
+
+And then you can install [`Twitter_Follower_Count.user.js`](https://twitter.com/NabiKAZ/Twitter-Follower-Count/raw/main/Twitter_Follower_Count.user.js) file with drag and drop file on your browser, or add the script by dashboard of Tampermonkey.
+
+## Screenshot
+![Twitter Follower Count Screenshot](https://github.com/NabiKAZ/Twitter-Follower-Count/assets/246721/e1ba50c2-f4df-4326-9656-7a5c50edabe6)
+
+## Donation
+If this project was useful for you and you are willing, you can make me happy by giving a Star at the top of this GitHub page. Also this is my wallet address for Donate:
+
+USDT (TRC20): `TEHjxGqu5Y2ExKBWzArBJEmrtzz3mgV5Hb`
---
+++ Twitter_Follower_Count.user.js
@@ -0,0 +1,136 @@
+// ==UserScript==
+// @name         Twitter Follower Count
+// @namespace    https://github.com/NabiKAZ/Twitter-Follower-Count
+// @version      0.1.0
+// @description  Display the number of followers of Twitter users
+// @author       Nabi K.A.Z. <nabikaz@gmail.com> | www.nabi.ir | @NabiKAZ
+// @match        https://twitter.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Array to store all users
+    var allUsers = [];
+
+    // Save references to the original methods
+    var originalSend = XMLHttpRequest.prototype.send;
+
+    // Override the send method
+    XMLHttpRequest.prototype.send = function() {
+        // Save a reference to the current instance
+        var xhr = this;
+
+        // Add an event listener to the load event
+        xhr.addEventListener('load', function() {
+            // Parse the response text as JSON
+            var responseJSON = JSON.parse(xhr.responseText);
+
+            // Extract user data from the response
+            var users = getNames(responseJSON, 'screen_name');
+
+            // Iterate over the users
+            users.forEach(function(user) {
+                // Check if the user object contains the required properties
+                if (!user.hasOwnProperty('name') || !user.hasOwnProperty('screen_name') || !user.hasOwnProperty('followers_count') || !user.hasOwnProperty('friends_count')) return;
+
+                // Check if the user is already in the allUsers array
+                var isDuplicate = allUsers.some(item => item.screen_name === user.screen_name);
+                if (!isDuplicate) {
+                    // Add the user to the allUsers array
+                    allUsers.push({
+                        'name': user.name,
+                        'screen_name': user.screen_name,
+                        'followers_count': user.followers_count,
+                        'friends_count': user.friends_count,
+                    });
+                }
+            });
+        });
+
+        // Call the original send method
+        originalSend.apply(xhr, arguments);
+
+    };
+
+    // Recursive function to extract names from an object
+    function getNames(obj, name, result = []) {
+        for (var key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                if ("object" == typeof(obj[key])) {
+                    getNames(obj[key], name, result);
+                } else if (key == name) {
+                    result.push(obj);
+                }
+            }
+        }
+        return result;
+    }
+
+    // Main function to update follower count on Twitter profiles
+    function main() {
+        allUsers.forEach(function(user) {
+            // Find the profile links of the user
+            var linkElements = document.querySelectorAll('a[href="/' + user.screen_name + '"]');
+
+            // Iterate over the profile links
+            linkElements.forEach(function(linkElement) {
+                // Check if the count element already exists
+                var countElement = linkElement.querySelector('span.count-follower');
+                if (countElement) return;
+
+                // Find the parent element of the link
+                var parentElement = linkElement.parentNode;
+                if (!parentElement) return;
+
+                // Find the image element in the parent element
+                var imgElement = parentElement.querySelector('img[draggable=true]');
+                if (!imgElement) return;
+
+                // Update styles of parent
+                parentElement.style.overflow = 'inherit';
+                parentElement.style.clipPath = 'none';
+
+                // Find and update style of closest parent element
+                var closestElement = parentElement.closest('ul');
+                if (closestElement) {
+                    closestElement.style.overflow = 'inherit';
+                }
+
+                // Create and append the count element
+                var newSpanElement = document.createElement('span');
+                newSpanElement.className = 'count-follower';
+                newSpanElement.innerText = user.followers_count.toLocaleString('en-US');
+                newSpanElement.style.position = 'absolute';
+                newSpanElement.style.top = '-9px';
+                newSpanElement.style.left = '50%';
+                newSpanElement.style.transform = 'translate(-50%)';
+                newSpanElement.style.fontFamily = '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif';
+                newSpanElement.style.fontSize = '9px';
+                newSpanElement.style.fontWeight = '400';
+                newSpanElement.style.whiteSpace = 'nowrap';
+                newSpanElement.style.color = 'white';
+                newSpanElement.style.backgroundColor = '#1d9bf0';
+                newSpanElement.style.border = '1px solid white';
+                newSpanElement.style.borderRadius = '9999px';
+                newSpanElement.style.padding = '0px 4px 1px';
+
+                // Append the count element to the link element
+                linkElement.appendChild(newSpanElement);
+
+            });
+        });
+    }
+
+    // Call the main function on scroll event
+    document.addEventListener("scroll", (event) => {
+        main();
+    });
+
+    // Call the main function on page load
+    window.addEventListener('load', () => {
+        main();
+    }, false);
+
+})();
